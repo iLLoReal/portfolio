f6c10edbb54eb7ef1b61ff915d8b25142f60a180
olivier <olivier.laffon@epitech.eu>
2022-09-27 17:56:54 +0200

    Simple store de projets avec redux toolkit et les slices

---
+++ src/components/pages/Projects/projectsSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { projectType } from './Projects';
+
+export interface projectsSliceState {
+  projects: projectType[];
+  status: 'idle' | 'rejected' | 'loading'
+}
+
+const initialState: projectsSliceState = {
+  projects: [{
+    id: 0,
+    title: 'Portfolio',
+    stack: ['React', 'TypeScript']
+  },{
+    id: 1,
+    title: 'RFP',
+    stack: ['React', 'TypeScript', 'Strapi', 'Tauri']
+  }],
+  status: 'idle'
+}
+
+const projectsSlice = createSlice({
+  name: 'projects',
+  initialState,
+  reducers: {
+    addProject(state: projectsSliceState, action: PayloadAction<projectType>) {
+      if (!state.projects.includes(action.payload)) {
+        state.projects.push(action.payload);
+      }
+    },
+  }
+});
+
+export const { addProject } = projectsSlice.actions
+
+export default projectsSlice.reducer
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { RouterProvider as Router } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import Projects from "./components/pages/Projects/Projects";
+import { Provider } from 'react-redux';
+import store from './state/store';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/home',
+    element: <App />,
+  },
+  {
+    path: '/projects',
+    element: <Projects />,
+  }
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+reportWebVitals(console.log);
